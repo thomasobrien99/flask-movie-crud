717aa21a5ea5e5ae93385b421e1fe982c4e07be4
Thomas O'Brien <thomasobrien99@gmail.com>
2016-06-27 15:17:47 -0700

    movies index page works, getting data from 'flask-movie-app' db

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, url_for, request, redirect, render_template
 from flask_modus import Modus
 
+from models.shared import db
 from models.movie import Movie
 
 
 app = Flask(__name__)
 
+'''WHAT IS ALL THIS DOING?'''
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask_movie_app'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.url_map.strict_slashes = False
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+''' '''
+
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+# Movie Routes
+@app.route('/movies')
+def index_movie():
+	movies = Movie.query.all();
+	return render_template('movies/index.html', movies=movies)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=3000);
--- models/movie.py
+++ models/movie.py
@@ -1,2 +1,10 @@
-class Movie:
-	pass
+from models.shared import db
+
+class Movie(db.Model):
+	__tablename__ = 'movies'
+
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.Text())
+	# director_id = db.Column(db.Integer)
+	def __init__ (self, title, director):
+		self.title = title
